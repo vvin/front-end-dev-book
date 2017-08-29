19f085d2c4323ef723a29b70ccad526bab39c4a1
Vin <vin@Vins-MacBook-Pro.local>
2017-08-29 18:21:02 -0500

    Finished Chapter 7

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -31,6 +35,7 @@ function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 
@@ -41,6 +46,17 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if(event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
@@ -48,6 +64,27 @@ function initializeEvents() {
     addThumbClickHandler(thumbnails[i]);
   } */
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler()
+}
+
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+  frame.classList.remove(TINY_EFFECT_CLASS);
+}, 50);
+  /*setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS):
+  }, 50);
+*/
 }
 
 /* Initialize code
